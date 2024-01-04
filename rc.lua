baddb5f18deea90825218ea6f76e1507bab656da
Eduardo Alencar <ealencar10@gmail.com>
2024-01-04 15:15:40 -0300

    feat: altera tag table para workflow cotidiano

--- rc.lua
+++ rc.lua
@@ -212,7 +212,7 @@ awful.screen.connect_for_each_screen(function(s)
     set_wallpaper(s)
 
     -- Each screen has its own tag table.
-    awful.tag({ "[1]", "[2]", "[3]", "[4]", "[5]", "[6]", "[7]", "[8]", "[9]", "[10]", "[11]", "[12]", "[13]" }, s, awful.layout.layouts[1])
+    awful.tag({ "[1]term", "[2]AG", "[3]BU", "[4]other", "[5]", "[6]", "[7]", "[8]", "[9]", "[10]", "[11]", "[12]", "[13]" }, s, awful.layout.layouts[1])
 
     -- Create a promptbox for each screen
     s.mypromptbox = awful.widget.prompt()
