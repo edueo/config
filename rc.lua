6829ddd3ee641be5aa73a0402d5fbef92d7c5071
Eduardo Alencar <ealencar10@gmail.com>
2024-03-26 18:16:06 -0300

    feat: tags

--- rc.lua
+++ rc.lua
@@ -212,7 +212,7 @@ awful.screen.connect_for_each_screen(function(s)
     set_wallpaper(s)
 
     -- Each screen has its own tag table.
-    awful.tag({ "www", "term", "ag", "bu", "ft", "other" }, s, awful.layout.layouts[1])
+    awful.tag({ "desk1", "desk2", "desk3", "desk4", "desk5", "desk6" }, s, awful.layout.layouts[1])
 
     -- Create a promptbox for each screen
     s.mypromptbox = awful.widget.prompt()
