4b17f9bad46cc9bde2a1ab2042413313dd7da288
Eduardo Alencar <ealencar10@gmail.com>
2024-04-24 19:22:23 -0300

    feat: kitty como terminal padrão

--- rc.lua
+++ rc.lua
@@ -55,7 +55,7 @@ end
 beautiful.init(gears.filesystem.get_themes_dir() .. "zenburn/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
-terminal = "terminator"
+terminal = "kitty"
 editor = os.getenv("EDITOR") or "vim"
 editor_cmd = terminal .. " -e " .. editor
 
